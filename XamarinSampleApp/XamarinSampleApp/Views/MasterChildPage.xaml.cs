40cc4bf2bc00a48fded0506a6317f95859f20cd1
User <User@USER-PC>
2018-01-22 12:00:09 +0530

    Solution Updated

---
+++ XamarinSampleApp/XamarinSampleApp/Views/MasterChildPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinSampleApp.Views
+{
+    public partial class MasterChildPage : MasterDetailPage
+    {
+        public MasterChildPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(PersonalDetailPage)));
+
+        }
+    }
+}
---
+++ XamarinSampleApp/XamarinSampleApp/Views/PersonalDetailPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinSampleApp.Views
+{
+    public partial class PersonalDetailPage : ContentPage
+    {
+        public PersonalDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
